92df2953365c4e0bc5ff48d0037aa605ca3aed7c
HomerJR80 <143743845+HomerJR80@users.noreply.github.com>
2026-05-27 23:07:26 +0200

    updated const.py

--- custom_components/oil_tank_manager/const.py
+++ custom_components/oil_tank_manager/const.py
@@ -1,3 +1,15 @@
 """Konstanten für Öltank Manager."""
 
 DOMAIN = "oil_tank_manager"
+
+# Config Schlüssel
+CONF_TANK_SIZE = "tank_size"
+CONF_ENERGY_SENSOR = "energy_sensor"
+
+# Services
+SERVICE_ADD_REFILL = "add_refill"
+SERVICE_ADD_CALIBRATION = "add_calibration"
+SERVICE_DELETE_ENTRY = "delete_entry"
+
+# Standardwerte
+DEFAULT_TANK_SIZE = 3000
